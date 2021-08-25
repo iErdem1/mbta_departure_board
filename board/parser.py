a92ba96c21aaf6f462e7f9d952144cf11644f7a7
iErdem1 <ahmedihsanerdem@gmail.com>
2021-08-25 13:47:34 -0400

    Trip Data key error handled.

--- board/parser.py
+++ board/parser.py
@@ -28,21 +28,33 @@ class Parser:
         return trip_id
     
     @staticmethod
-    def get_trip_detail(id,base_url):
-        url = f'{base_url}/trips?filter[id]={id}'
-        res = requests.get(url=url)
-        data = res.json()['data']
-        return data
+    def get_trip_detail(trip_id,base_url):
+        try:
+            url = f'{base_url}/trips?filter[id]={trip_id}'
+            res = requests.get(url=url)
+            data = res.json()['data'][0]
+            return data
+        except KeyError:
+            print("API data broken.")
     
     @staticmethod
     def get_destination(data):
-        destination = data[0]['attributes']['headsign']
-        return destination
-    
+        try:
+            destination = data['attributes']['headsign']
+            return destination
+        except TypeError:
+            destination = "Loading..."
+            print("NoneType Error. Trip Data broken.")
+            return destination   
     @staticmethod
     def get_train_number(data):
-        train_number = data[0]['attributes']['name']
-        return train_number
+        try:
+            train_number = data['attributes']['name']
+            return train_number
+        except TypeError:
+            destination = "Loading..."
+            print("NoneType Error. Trip Data broken.")
+            return destination
 
     @staticmethod
     def get_status(prediction_data):
